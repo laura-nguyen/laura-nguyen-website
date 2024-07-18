6226e3f0fcad9fe5f1be30ffcc0243fd86117bd0
Laura Nguyen <hello@lauranguyen.me>
2024-07-17 21:36:42 -0400

    added navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import Header from "./components/Header/Header";
 import "./App.scss";
 
 function App() {
@@ -8,26 +10,9 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import "./Header.scss";
+import lotusLogo from "./../../assets/icons/logo-lotus.png";
+import exitIcon from "./../../assets/icons/exit.svg";
+import hamburgerMenuIcon from "./../../assets/icons/hamburger-menu.svg";
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const handleNavLinkClick = () => {
+    document.getElementById("sidebar-active").checked = false;
+  };
+
+  return (
+    <header className="header">
+      <NavLink to="/" className="header__logo">
+        <img
+          className="header__logo-icon"
+          src={lotusLogo}
+          alt="vector icon of a lotus flower"
+        />
+        <span>
+          yog<span className="logo__title--coral">ai</span>nstruct
+        </span>
+      </NavLink>
+      <nav>
+        <input type="checkbox" id="sidebar-active" />
+        <label htmlFor="sidebar-active" className="open-sidebar-button">
+          <img src={hamburgerMenuIcon} alt="hamburger menu icon" />
+        </label>
+        <label id="overlay" htmlFor="sidebar-active"></label>
+        <div className="links-container">
+          <label htmlFor="sidebar-active" className="close-sidebar-button">
+            <img src={exitIcon} alt="exit icon" />
+          </label>
+
+          <div className="nav__links">
+            <NavLink
+              to="/poses"
+              className="nav__link--non-button"
+              onClick={handleNavLinkClick}
+            >
+              Learn Yoga
+            </NavLink>
+            <div className="nav__link-wrapper--cta">
+              <NavLink to="/instructions">
+                <button className="nav__link--cta">Start Session</button>
+              </NavLink>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
